d2557fcc8b4ef8843f72242f81a3eb7a3c87cc60
devydk <98907568+DevydSilva@users.noreply.github.com>
2022-03-02 14:24:26 -0300

    commit

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import { View, Text, TextInput} from "react-native";
-import { Formik} from "formik"
-import Container from './styles';
+//import Container from './styles';
 
 export default () =>{
   return (
-    <Container>
+    <View>
       <Text>Ola Mundo</Text>
-      <Formik
-        initialValues={{ title: '', body: '', rating: ''}}
-        onSubmit={(values) => {
-
-        }}
-      
-      >
-        {(props) => (
-          <View>
-            <TextInput />
-          </View>
-        )}
-      </Formik>
-    </Container>
+    </View>
   )
 }
